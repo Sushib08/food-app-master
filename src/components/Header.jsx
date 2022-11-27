3f31e6bf0a8c25b75465fe977126c5fb75a8de87
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-11-27 11:33:50 +0100

    config firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Logo from "./img/logo.png";
 import Avatar from "./img/avatar.png";
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebase.config";
+
 import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const login = () => {
+    const response = signInWithPopup(firebaseAuth, provider);
+    console.log(response);
+  };
+
   return (
     <header className=" fixed z-50 w-screen">
       {/* desktop & tablet */}
       <div className=" hidden md:flex w-full h-full items-center justify-between p-4">
-        <div className=" flex items-center gap-2">
-          <img src={Logo} className=" w-10 object-cover" alt="logo" />
-          <p className=" text-headingColor text-xl font-bold">city</p>
-        </div>
+        <motion.div whileTap={{ scale: 0.8 }}>
+          <Link to={"/"} className=" flex items-center gap-2">
+            <img src={Logo} className=" w-10 object-cover" alt="logo" />
+            <p className=" text-headingColor text-xl font-bold">city</p>
+          </Link>
+        </motion.div>
         <div className=" flex items-center gap-8">
           <ul className=" flex items-center gap-8">
             <li className=" text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
@@ -33,11 +49,15 @@ export const Header = () => {
               <p className=" text-xs text-white font-semibold">5</p>
             </div>
           </div>
-          <img
-            src={Avatar}
-            className=" w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl"
-            alt="user-profil"
-          />
+          <div className=" relative ">
+            <motion.img
+              whileTap={{ scale: 0.8 }}
+              src={Avatar}
+              className=" w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl cursor-pointer"
+              alt="user-profil"
+              onClick={login}
+            />
+          </div>
         </div>
       </div>
 
