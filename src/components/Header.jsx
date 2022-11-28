6c7dec01c27e57306cce00650c6747ac62a5687b
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-11-28 16:48:31 +0100

    making MainContainer part1 + responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,6 +51,7 @@ export const Header = () => {
             <p className=" text-headingColor text-xl font-bold">ChicKing</p>
           </Link>
         </motion.div>
+
         <div className=" flex items-center gap-8">
           <motion.ul
             initial={{ opacity: 0, x: 200 }}
@@ -58,30 +59,47 @@ export const Header = () => {
             exit={{ opacity: 0, x: 200 }}
             className=" flex items-center gap-8"
           >
-            <li className=" text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className=" text-base text-textColor hover:text-headingColor 
+            duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Home
             </li>
-            <li className=" text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className=" text-base text-textColor hover:text-headingColor 
+            duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Menu
             </li>
-            <li className=" text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className=" text-base text-textColor hover:text-headingColor 
+            duration-100 transition-all ease-in-out cursor-pointer"
+            >
               A propos de nous
             </li>
-            <li className=" text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className=" text-base text-textColor hover:text-headingColor 
+            duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Service
             </li>
           </motion.ul>
           <div className=" relative flex items-center justify-center">
             <MdShoppingBasket className=" text-textColor text-2xl cursor-pointer" />
-            <div className=" absolute -top-2 -right-2 w-6 h-6 rounded-full bg-cartNumBg flex items-center justify-center">
+            <div
+              className=" absolute -top-2 -right-2 w-6 h-6 rounded-full 
+            bg-cartNumBg flex items-center justify-center"
+            >
               <p className=" text-xs text-white font-semibold">5</p>
             </div>
           </div>
+
           <div className=" relative ">
             <motion.img
               whileTap={{ scale: 0.8 }}
               src={user ? user.photoURL : Avatar}
-              className=" w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl cursor-pointer rounded-full"
+              className=" w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl 
+              cursor-pointer rounded-full"
               alt="user-profil"
               onClick={login}
             />
@@ -90,17 +108,24 @@ export const Header = () => {
                 initial={{ opacity: 0, scale: 0.6 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.6 }}
-                className=" w-56 bg-grey-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2"
+                className=" w-56 bg-grey-50 shadow-xl rounded-lg flex flex-col 
+                absolute top-12 right-0 px-4 py-2"
               >
                 {user && user.email === "maevalagrost@gmail.com" && (
                   <Link to={"/createItem"}>
-                    <p className=" px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <p
+                      className=" px-4 py-2 flex items-center gap-3 cursor-pointer
+                     hover:bg-slate-200 transition-all duration-100 ease-in-out
+                     text-textColor text-base"
+                    >
                       Nouvel article <MdAdd />
                     </p>
                   </Link>
                 )}
                 <p
-                  className=" px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base"
+                  className=" px-4 py-2 flex items-center gap-3 cursor-pointer
+                   hover:bg-slate-200 transition-all duration-100 ease-in-out
+                   text-textColor text-base"
                   onClick={logout}
                 >
                   Se déconnecter <MdLogout />
@@ -113,15 +138,27 @@ export const Header = () => {
 
       {/* mobile */}
       <div className=" flex items-center justify-between md:hidden w-full h-full">
+        <div className=" relative flex items-center justify-center">
+          <MdShoppingBasket className=" text-textColor text-2xl cursor-pointer" />
+          <div
+            className=" absolute -top-2 -right-2 w-6 h-6 rounded-full 
+          bg-cartNumBg flex items-center justify-center"
+          >
+            <p className=" text-xs text-white font-semibold">5</p>
+          </div>
+        </div>
+
         <Link to={"/"} className=" flex items-center gap-2">
           <img src={Logo} className=" w-10 object-cover" alt="logo" />
           <p className=" text-headingColor text-xl font-bold">ChicKing</p>
         </Link>
+
         <div className=" relative ">
           <motion.img
             whileTap={{ scale: 0.8 }}
             src={user ? user.photoURL : Avatar}
-            className=" w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl cursor-pointer rounded-full"
+            className=" w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl 
+            cursor-pointer rounded-full"
             alt="user-profil"
             onClick={login}
           />
@@ -130,31 +167,54 @@ export const Header = () => {
               initial={{ opacity: 0, scale: 0.6 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.6 }}
-              className=" w-56 bg-grey-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2"
+              className=" w-56 bg-grey-50 shadow-xl rounded-lg flex flex-col 
+              absolute top-12 right-0 px-4 py-2"
             >
               {user && user.email === "maevalagrost@gmail.com" && (
                 <Link to={"/createItem"}>
-                  <p className=" ml-4 flex items-center gap-3 cursor-pointer bg-grey-200 hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base">
+                  <p
+                    className=" ml-4 flex items-center gap-3 cursor-pointer 
+                  bg-grey-200 hover:bg-slate-200 transition-all duration-100 ease-in-out 
+                  text-textColor text-base"
+                  >
                     Nouvel article <MdAdd />
                   </p>
                 </Link>
               )}
               <ul className=" flex flex-col px-4">
-                <li className=" text-base text-textColor  hover:bg-slate-200 hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                <li
+                  className=" text-base text-textColor  hover:bg-slate-200 
+                hover:text-headingColor duration-100 transition-all ease-in-out 
+                cursor-pointer"
+                >
                   Home
                 </li>
-                <li className=" text-base text-textColor  hover:bg-slate-200 hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                <li
+                  className=" text-base text-textColor  hover:bg-slate-200 
+                hover:text-headingColor duration-100 transition-all ease-in-out 
+                cursor-pointer"
+                >
                   Menu
                 </li>
-                <li className=" text-base text-textColor  hover:bg-slate-200 hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                <li
+                  className=" text-base text-textColor  hover:bg-slate-200 
+                hover:text-headingColor duration-100 transition-all ease-in-out 
+                cursor-pointer"
+                >
                   A propos de nous
                 </li>
-                <li className=" text-base text-textColor  hover:bg-slate-200 hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+                <li
+                  className=" text-base text-textColor  hover:bg-slate-200 
+                hover:text-headingColor duration-100 transition-all ease-in-out 
+                cursor-pointer"
+                >
                   Service
                 </li>
               </ul>
               <p
-                className=" flex items-center gap-3 hover:shadow-lg cursor-pointer justify-center bg-grey-400 hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base"
+                className=" flex items-center gap-3 hover:shadow-lg cursor-pointer 
+                justify-center bg-grey-400 hover:bg-slate-200 transition-all duration-100 
+                ease-in-out text-textColor text-base"
                 onClick={logout}
               >
                 Se déconnecter <MdLogout />
