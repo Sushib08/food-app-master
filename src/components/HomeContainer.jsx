737f948ec90f68cd08085192122d48fbe3329cc4
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-12-07 17:27:43 +0100

    finished homecomponent

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Delivery from "./img/delivery.png";
-import HeroBg from "./img/heroBg.png";
+import Delivery from "../img/delivery.png";
+import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/data";
 
 export const HomeContainer = () => {
   return (
@@ -8,14 +9,8 @@ export const HomeContainer = () => {
       className=" grid grid-cols-1 md:grid-cols-2 gap-2 w-full"
       id="home"
     >
-      <div
-        className=" py-2 flex-1 flex flex-col items-start justify-center 
-    gap-4"
-      >
-        <div
-          className=" flex items-center gap-2 justify-center
-       bg-orange-100 px-2 py-1 rounded-full"
-        >
+      <div className=" py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className=" flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className=" text-base text-orange-500 font-semibold">Livraison</p>
           <div className=" w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img
@@ -26,17 +21,14 @@ export const HomeContainer = () => {
           </div>
         </div>
 
-        <p
-          className=" text-[2.5rem] font-bold tracking-wide text-headingColor 
-      lg:text-[4.25rem]"
-        >
+        <p className=" text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
           Livraison Rapide Dans{" "}
-          <span className=" text-orange-500 text-[3rem] lg:text-[5rem]">
+          <span className=" text-orange-600 text-[3rem] lg:text-[5rem]">
             Ta Ville
           </span>
         </p>
 
-        <p className=" text-base text-textColor text-center md:text-left md:w-[80]">
+        <p className=" text-base text-textColor text-center md:text-left md:w-[80%]">
           N'hésitez plus nous livrons à Lyon et ses environs !<br />
           Livraison gratuite{" "}
           <span className=" text-orange-500 font-semibold">
@@ -61,17 +53,43 @@ export const HomeContainer = () => {
 
         <button
           type="button"
-          className=" bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 
-        rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 md:w-auto"
+          className=" bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Commander maintenant
         </button>
       </div>
-      <div className=" py-2 flex-1 flex items-center">
-        <img src={HeroBg} className=' ml-auto h:420 w-full lg:w-auto lg:h-650' alt="hero-Bg" />
+      <div className=" py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroBg}
+          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+          alt="hero-Bg"
+        />
 
-        <div className=" w-full h-full absolute flex items-center justify-center">
-
+        <div className=" w-full h-full absolute top-0 left-0 flex items-center justify-center 
+        xl:px-32 py-4 gap-4 flex-wrap">
+          {heroData &&
+            heroData.map((items) => (
+              <div
+                key={items.id}
+                className=" lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl 
+                flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={items.imageSrc}
+                  className=" w-20 lg:w-40 -mt-10 lg:-mt-20"
+                  alt={items.name}
+                />
+                <p className=" text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {items.name}
+                </p>
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {items.decp}
+                </p>
+                <p className=" text-sm font-semibold text-headingColor">
+                  {items.price} <span className=" text-xs text-red-600">€</span>
+                </p>
+              </div>
+            ))}
         </div>
       </div>
     </section>
---
+++ src/utils/data.js
@@ -0,0 +1,36 @@
+
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3.png";
+import Fi1 from "../img/fi1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Glace",
+    decp: "Chocolat & vanille",
+    price: "3.5",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Fraise",
+    decp: "Fraise des bois",
+    price: "9.95",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Chicken Kebab",
+    decp: "Assiette de Kebab",
+    price: "8.5",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Poisson",
+    decp: "Assiette de poisson",
+    price: "6.75",
+    imageSrc: Fi1,
+  },
+];
